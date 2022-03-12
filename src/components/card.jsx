386b06c4d52463b7c78e6d5c6fb2bec8a2f1ea3b
anshul <anshullaikar@gmail.com>
2022-03-12 22:36:23 +0530

    made card component independent

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React from "react";
+import styled from "styled-components";
 
+const CardDiv = styled.ul`
+    margin-top: 2rem;
+    margin-left: 5ch;
+    margin-right: 5ch;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    padding: 2rem;
+    border-radius: 10px;
+`;
+const CardTitle = styled.h2`
+    font-weight: 600;
+    font-size: 1.25rem;
+    padding-bottom: 0.25rem;
+`;
+const CardAuthor = styled.h5`
+    padding-top: 1rem;
+    font-style: italic;
+    font-weight: 300;
+`;
 class Card extends React.Component {
     render() {
         return (
-            <div className="card">
-                <div className="card-body">
-                    <h2>{this.props.title}</h2>
-                    <p>{this.props.body}</p>
-                    <h5>Written by: {this.props.author}</h5>
-                </div>
-            </div>
+            <CardDiv>
+                <CardTitle>{this.props.title}</CardTitle>
+                <p>{this.props.body}</p>
+                <CardAuthor>Written by: {this.props.author}</CardAuthor>
+            </CardDiv>
         );
     }
 }
